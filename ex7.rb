e95646a8020f30de14d67e7d74399995e374cc66
Benjamin R Doerr <tiggerizzy@gmail.com>
2011-09-04 14:38:44 -0400

    Exercies 6-9

---
+++ ex7.rb
@@ -0,0 +1,27 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went."
+puts "." * 10 # Puts 10 .
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# notice how we are using print unstead of puts. change it to puts
+# and see what happens.
+# Print doesn't put a new line.
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# this just is polite use of the terminal, try removing it
+# Leaves off a new line
+puts
---
+++ ex9.rb
@@ -0,0 +1,19 @@
+# Here's some new stange stuff, rememeber type it exactly
+
+days = "Mon Tue Web Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: ", days
+puts "Here are the months: ", months
+
+puts <<PARAGRAPH
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAPH
+
+puts """
+Test of three double quotes
+Why mention it then don't use it.
+"""
