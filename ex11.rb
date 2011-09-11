3f8b387d26bf16eb4f7e141093911f6e6f153956
Benjamin R Doerr <tiggerizzy@gmail.com>
2011-09-11 15:49:24 -0400

    Exercise 10 and 11

---
+++ ex11.rb
@@ -0,0 +1,11 @@
+print "How old are you? "
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weigh? "
+weight = gets.chomp()
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+# gets gets a line from input
+# chomp removes the last character, in our line instance it removes the new line character from when you hit enter.
